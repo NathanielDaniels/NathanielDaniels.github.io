5d021e1705dbb4fa3a4de36bc32d5899f5528582
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-02-28 15:19:55 -0600

    first commit

---
+++ JS/script.js
@@ -0,0 +1,36 @@
+mainTitle = document.getElementById('menu-title')
+
+mainTitle.addEventListener('click', function () {
+  mainTitle.innerHTML = 'Welcome!'
+})
+
+// Greeting-Loop =======================================
+var text = ['Web Designer', 'Web Enthusiast', 'Front-End Developer']
+var counter = 0
+var elem = $('#greeting')
+setInterval(change, 3000)
+function change () {
+  elem.fadeOut(function () {
+    elem.html(text[counter])
+    counter++
+    if (counter >= text.length) {
+      counter = 0
+    }
+    elem.fadeIn()
+  })
+}
+
+// Bounce Animation ====================================
+// $(document).ready(function () {
+//   $('.bounce').effect('bounce', { times: 3 }, 600)
+// }, 1000)
+
+var docLink = $('#bounce')
+
+setInterval(function () {
+  docLink.effect('bounce', { times: 1 }, 1000)
+})
+
+// setInterval(function () {
+//   $('.bounce').effect('bounce', { times: 3 }, 600)
+// }, 1000)
