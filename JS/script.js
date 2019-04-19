3a5198a0120d3cafb610bcc3355a8fc53e7ee680
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2019-04-19 07:46:39 -0500

    added function to JS to test search bar animation

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,21 @@
+// var l = ["▁", "▂", "▃", "▄", "▅", "▆", "▇", "█"];
+// var x = new AudioContext();
+// var a = x.createAnalyser();
+// a.fftSize = 32;
+// var d = new Uint8Array(16);
+// navigator.mediaDevices.getUserMedia({ audio: true }).then(s => {
+//   x.createMediaStreamSource(s).connect(a);
+//   z();
+// });
+
+// function z() {
+//   setTimeout(z, 40);
+//   a.getByteFrequencyData(d);
+//   var s = [];
+//   d.forEach(v => s.push(l[Math.floor((v / 255) * 8)]));
+//   location.hash = document.title = s.join("");
+// }
+
 // Title SVG Animation ======================
 $(function() {
   $("#name-svg").load("/titleName-anim/name-svg.html");
