797b888dc5d4256224111130132ca0002fd8a12b
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-08-06 10:40:35 -0500

    added progress bar to top of website

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,17 @@
+//! When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
 //! skills icon hover  ======================
 
 const skillsUl = document.querySelectorAll(".skills-list > li");
