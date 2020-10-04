135cd80b9010241c7159ac20c13f0bc8b8e4efa4
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-10-04 10:30:27 -0500

    completely removed canvas bubble animations due to performance issues

--- JS/script.js
+++ JS/script.js
@@ -152,6 +152,7 @@ function hamburgerAnimation() {
   });
 }
 hamburgerAnimation();
+
 //! Greeting-Loop Animation (JQuery)================================
 
 function greetingLoop() {
@@ -181,7 +182,6 @@ function greetingLoop() {
 greetingLoop();
 
 //! Sidebar location change (screen size) ====
-//? Need to fix new z-index issue on middle two sections
 
 // function sidebarPositionChange() {
 //   const sidebar = document.querySelector(".sidebar");
@@ -206,8 +206,8 @@ greetingLoop();
 //   });
 // }
 // sidebarPositionChange();
-//! skills icon hover  ======================
 
+//! Skills Hover Color Change (About)
 function skillsHover() {
   const skillsUl = document.querySelectorAll(".skills-list > li");
   const skillsLi = document.querySelectorAll(".skills-list > li > i");
@@ -227,16 +227,18 @@ function skillsHover() {
 }
 skillsHover();
 
-//! PORTFOLIO CLICK FUNCTION (JQuery)=====================
-//?flipping cards
-// $(function () {
-//   $(".card").click(function () {
-//     $(this)
-//       .toggleClass("is-flipped")
-//       .siblings(".is-flipped")
-//       .removeClass("is-flipped");
-//   });
-// });
+//! Tilt Animation (projects)
+//!=================================
+function tiltAnimation() {
+  VanillaTilt.init(document.querySelector(".cardImg"), {
+    max: 25,
+    speed: 400,
+  });
+
+  //It also supports NodeList
+  VanillaTilt.init(document.querySelectorAll(".cardImg"));
+}
+tiltAnimation();
 
 //! Contact Section - Floating Box Animation (Completely Random) ====
 let boxSize = () => {
@@ -255,287 +257,16 @@ let boxSize = () => {
 };
 boxSize();
 
-//! Tilt Animation
-
-function tiltAnimation() {
-  VanillaTilt.init(document.querySelector(".cardImg"), {
-    max: 25,
-    speed: 400,
-  });
-
-  //It also supports NodeList
-  VanillaTilt.init(document.querySelectorAll(".cardImg"));
-}
-
-tiltAnimation();
-
-//!================================================
-//! Floating Bubbles!!!
-
-// if (window.innerWidth >= 1200) {
-//   //? Add everything from floating bubbles into this if statement to stop mobile load.
-//   let canvas = document.querySelector("canvas");
-//   canvas.width = document.body.clientWidth;
-//   canvas.height = document.body.clientHeight;
-//   let ctx = canvas.getContext("2d");
-//   let count = canvas.height;
-//   let bubbles = [];
-//   let bubbleCount = 20;
-//   let bubbleSpeed = 1;
-//   let popLines = 6;
-//   let popDistance = 40;
-//   var mouseOffset = {
-//     x: 0,
-//     y: 0,
-//   };
-
-//   // --------------
-//   //! Animation Loop
-//   // --------------
-
-//   function animate() {
-//     // ------------
-//     //! Clear Canvas
-//     // ------------
-
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//     // ------------
-//     //! Draw Bubbles
-//     // ------------
-
-//     ctx.beginPath();
-//     for (var i = 0; i < bubbles.length; i++) {
-//       // first num = distance between waves
-//       // second num = wave height
-//       // third num = move the center of the wave away from the edge
-//       bubbles[i].position.x =
-//         Math.sin(bubbles[i].count / bubbles[i].distanceBetweenWaves) * 8 +
-//         bubbles[i].xOff;
-//       bubbles[i].position.y = bubbles[i].count;
-//       bubbles[i].render();
-
-//       if (bubbles[i].count < 0 - bubbles[i].radius) {
-//         bubbles[i].count = canvas.height + bubbles[i].yOff;
-//       } else {
-//         bubbles[i].count -= bubbleSpeed;
-//       }
-//     }
-
-//     // ---------------
-//     //! On Bubble Hover
-//     // ---------------
-
-//     for (var i = 0; i < bubbles.length; i++) {
-//       if (
-//         mouseOffset.x > bubbles[i].position.x - bubbles[i].radius &&
-//         mouseOffset.x < bubbles[i].position.x + bubbles[i].radius
-//       ) {
-//         if (
-//           mouseOffset.y > bubbles[i].position.y - bubbles[i].radius &&
-//           mouseOffset.y < bubbles[i].position.y + bubbles[i].radius
-//         ) {
-//           for (var a = 0; a < bubbles[i].lines.length; a++) {
-//             popDistance = bubbles[i].radius * 0.5;
-//             bubbles[i].lines[a].popping = true;
-//             bubbles[i].popping = true;
-//           }
-//         }
-//       }
-//     }
-
-//     window.requestAnimationFrame(animate);
-//   }
-
-//   window.requestAnimationFrame(animate);
-
-//   // ------------------
-//   //! Bubble Constructor
-//   // ------------------
-
-//   var createBubble = function () {
-//     this.position = { x: 0, y: 0 };
-//     this.radius = 8 + Math.random() * 12;
-//     this.xOff = Math.random() * canvas.width - this.radius;
-//     this.yOff = Math.random() * canvas.height;
-//     this.distanceBetweenWaves = 50 + Math.random() * 40;
-//     this.count = canvas.height + this.yOff;
-//     // this.color = "#8bc9ee";
-//     this.color = "hsl(271, 76%, 20%)";
-//     this.lines = [];
-//     this.popping = false;
-//     this.maxRotation = 85;
-//     this.rotation =
-//       Math.floor(Math.random() * (this.maxRotation - this.maxRotation * -1)) +
-//       this.maxRotation * -1;
-//     this.rotationDirection = "forward";
-
-//     //! Populate Lines
-//     for (var i = 0; i < popLines; i++) {
-//       var tempLine = new createLine();
-//       tempLine.bubble = this;
-//       tempLine.index = i;
-
-//       this.lines.push(tempLine);
-//     }
-
-//     this.resetPosition = function () {
-//       this.position = { x: 0, y: 0 };
-//       this.radius = 8 + Math.random() * 12;
-//       this.xOff = Math.random() * canvas.width - this.radius;
-//       this.yOff = Math.random() * canvas.height;
-//       this.distanceBetweenWaves = 50 + Math.random() * 40;
-//       this.count = canvas.height + this.yOff;
-//       this.popping = false;
-//     };
-
-//     //! Render the circles
-//     this.render = function () {
-//       if (this.rotationDirection === "forward") {
-//         if (this.rotation < this.maxRotation) {
-//           this.rotation++;
-//         } else {
-//           this.rotationDirection = "backward";
-//         }
-//       } else {
-//         if (this.rotation > this.maxRotation * -1) {
-//           this.rotation--;
-//         } else {
-//           this.rotationDirection = "forward";
-//         }
-//       }
-
-//       ctx.save();
-//       ctx.translate(this.position.x, this.position.y);
-//       ctx.rotate((this.rotation * Math.PI) / 180);
-
-//       if (!this.popping) {
-//         ctx.beginPath();
-//         // ctx.strokeStyle = "#8bc9ee";
-//         ctx.strokeStyle = "hsl(271, 76%, 20%)";
-//         ctx.lineWidth = 1;
-//         ctx.arc(0, 0, this.radius - 3, 0, Math.PI * 1.5, true);
-//         ctx.stroke();
-
-//         ctx.beginPath();
-//         ctx.arc(0, 0, this.radius, 0, Math.PI * 2, false);
-//         ctx.stroke();
-//       }
-
-//       ctx.restore();
-
-//       //! Draw the lines
-//       for (var a = 0; a < this.lines.length; a++) {
-//         if (this.lines[a].popping) {
-//           if (
-//             this.lines[a].lineLength < popDistance &&
-//             !this.lines[a].inversePop
-//           ) {
-//             this.lines[a].popDistance += 0.06;
-//           } else {
-//             if (this.lines[a].popDistance >= 0) {
-//               this.lines[a].inversePop = true;
-//               this.lines[a].popDistanceReturn += 1;
-//               this.lines[a].popDistance -= 0.03;
-//             } else {
-//               this.lines[a].resetValues();
-//               this.resetPosition();
-//             }
-//           }
-
-//           this.lines[a].updateValues();
-//           this.lines[a].render();
-//         }
-//       }
-//     };
-//   };
-
-//   // ----------------
-//   //! Populate Bubbles
-//   // ----------------
-
-//   for (var i = 0; i < bubbleCount; i++) {
-//     var tempBubble = new createBubble();
-
-//     bubbles.push(tempBubble);
-//   }
-
-//   // ----------------
-//   //! Line Constructor
-//   // ----------------
-
-//   function createLine() {
-//     this.lineLength = 0;
-//     this.popDistance = 0;
-//     this.popDistanceReturn = 0;
-//     this.inversePop = false; // When the lines reach full length they need to shrink into the end position
-//     this.popping = false;
-
-//     this.resetValues = function () {
-//       this.lineLength = 0;
-//       this.popDistance = 0;
-//       this.popDistanceReturn = 0;
-//       this.inversePop = false;
-//       this.popping = false;
-
-//       this.updateValues();
-//     };
-
-//     this.updateValues = function () {
-//       this.x =
-//         this.bubble.position.x +
-//         (this.bubble.radius + this.popDistanceReturn) *
-//           Math.cos((2 * Math.PI * this.index) / this.bubble.lines.length);
-//       this.y =
-//         this.bubble.position.y +
-//         (this.bubble.radius + this.popDistanceReturn) *
-//           Math.sin((2 * Math.PI * this.index) / this.bubble.lines.length);
-//       this.lineLength = this.bubble.radius * this.popDistance;
-//       this.endX = this.lineLength;
-//       this.endY = this.lineLength;
-//     };
-
-//     this.render = function () {
-//       this.updateValues();
-
-//       ctx.beginPath();
-//       // ctx.strokeStyle = "#8bc9ee";
-//       ctx.strokeStyle = "hsl(271, 76%, 20%)";
-//       ctx.lineWidth = 2;
-//       ctx.moveTo(this.x, this.y);
-//       if (this.x < this.bubble.position.x) {
-//         this.endX = this.lineLength * -1;
-//       }
-//       if (this.y < this.bubble.position.y) {
-//         this.endY = this.lineLength * -1;
-//       }
-//       if (this.y === this.bubble.position.y) {
-//         this.endY = 0;
-//       }
-//       if (this.x === this.bubble.position.x) {
-//         this.endX = 0;
-//       }
-//       ctx.lineTo(this.x + this.endX, this.y + this.endY);
-//       ctx.stroke();
-//     };
-//   }
-
-//   // ---------------
-//   //! Event Listeners
-//   // ---------------
-
-//   canvas.addEventListener("mousemove", mouseMove);
-
-//   function mouseMove(e) {
-//     mouseOffset.x = e.offsetX;
-//     mouseOffset.y = e.offsetY;
-//   }
-
-//   window.addEventListener("resize", function () {
-//     canvas.width = document.body.clientWidth;
-//     canvas.height = document.body.clientHeight;
+//! PORTFOLIO CLICK FUNCTION (JQuery)=====================
+//?flipping cards
+// $(function () {
+//   $(".card").click(function () {
+//     $(this)
+//       .toggleClass("is-flipped")
+//       .siblings(".is-flipped")
+//       .removeClass("is-flipped");
 //   });
-// }
+// });
 
 //! Contact form Label Animation (to legend) =================
 
