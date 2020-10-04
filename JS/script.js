c8d8f55c3aa7183cb5d7d9aab30581c8e930bb17
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-10-04 15:17:52 -0500

    working on project box animation

--- JS/script.js
+++ JS/script.js
@@ -230,13 +230,13 @@ skillsHover();
 //! Tilt Animation (projects)
 //!=================================
 function tiltAnimation() {
-  VanillaTilt.init(document.querySelector(".cardImg"), {
-    max: 25,
-    speed: 400,
+  VanillaTilt.init(document.querySelectorAll(".cardImg"), {
+    // max: 95,
+    // speed: 400,
+    // perspective: 500,
+    // glare: true,
+    // "max-glare": 1,
   });
-
-  //It also supports NodeList
-  VanillaTilt.init(document.querySelectorAll(".cardImg"));
 }
 tiltAnimation();
 
